3404e3ea59b62d11bfc407b8690d6fb195158097
Aot25922 <aottanapat25922@gmail.com>
2021-08-22 11:19:01 +0700

    first commit

---
+++ KaoFoodProject/src/main/java/backend/kaofood/kaofoodproject/KaoFoodProjectApplication.java
@@ -0,0 +1,13 @@
+package backend.kaofood.kaofoodproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaoFoodProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaoFoodProjectApplication.class, args);
+    }
+
+}
---
+++ KaoFoodProject/src/test/java/backend/kaofood/kaofoodproject/KaoFoodProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.kaofood.kaofoodproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaoFoodProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
