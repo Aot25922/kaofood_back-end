6030cd5a053aa1ca44f081b20d368f33d944bc30
Aot25922 <aottanapat25922@gmail.com>
2021-10-27 12:04:18 +0700

    Update Category bug

--- src/main/java/kao/backend/spring/contoller/CategoryController.java
+++ src/main/java/kao/backend/spring/contoller/CategoryController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin
+@RestController
 @RequestMapping("/category")
 public class CategoryController {
     @Autowired
