8379cbc64b79fe36bf0d194f3fe4381598f23abf
Aot25922 <aottanapat25922@gmail.com>
2021-11-02 08:46:52 +0700

    Add orderdetail of user API

--- src/main/java/kao/backend/spring/repository/OrderDetailRepository.java
+++ src/main/java/kao/backend/spring/repository/OrderDetailRepository.java
@@ -3,5 +3,8 @@ package kao.backend.spring.repository;
 import kao.backend.spring.model.OrderDetailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderDetailRepository extends JpaRepository<OrderDetailEntity , Integer> {
+    List<OrderDetailEntity> findAllByOrders_User_Id(int id);
 }
