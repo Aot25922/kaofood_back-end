76671c36651816b0512f50b5970bb244309c04b8
nithach2321 <56868125+nithach2321@users.noreply.github.com>
2021-12-02 14:45:13 +0700

    App 1.0.4 (Fix Bug User Delete) (#28)
    * Filter Account
    
    * Change Admin Method
    
    * Update image name and Add cart system
    
    * Edit Order system
    
    * Update userEntity and Edit User profile
    
    * Update Response and password API
    
    * Update Category bug
    
    * Bug TEst
    
    * Bug TEst2
    
    * Bug TEst3
    
    * Bug TEst4
    
    * Bug TEst5
    
    * Bug TEst6
    
    * Bug TEst7
    
    * Bug TEst7
    
    * Bug TEst8
    
    * Bug TEst9
    
    * Bug TEst10
    
    * Bug TEst11
    
    * Bug TEst12
    
    * Add orderdetail of user API
    
    * Update Dockerfile
    
    * Change get order user to OrderController.java
    
    * Change get order user to OrderController.java
    
    * Fixbug ordermanage
    
    * Update maxLifetime
    
    * [Update] Fix order
    
    * Update maxLifetime
    
    * Update maxLifetime
    
    * Update dburl & maxLifetime
    
    * Test
    
    * Edit maxLifetime
    
    * Edit RequestParam
    
    * Update Delete User
    
    Co-authored-by: Aot25922 <aottanapat25922@gmail.com>
    Co-authored-by: thanasapL <thanasap.lee@outlook.com>

--- src/main/java/kao/backend/spring/repository/OrderRepository.java
+++ src/main/java/kao/backend/spring/repository/OrderRepository.java
@@ -3,13 +3,17 @@ package kao.backend.spring.repository;
 import kao.backend.spring.model.OrderEntity;
 import kao.backend.spring.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<OrderEntity , Integer> {
-    OrderEntity findByUser_Id(int id);
-    OrderEntity findByUser(UserEntity user);
     OrderEntity findById(int id);
-
     List<OrderEntity> findAllByUser_Id(int id);
+    @Transactional
+    @Modifying
+    @Query("delete from OrderEntity o where o.user.id = ?1")
+    void deleteByUser_Id(int id);
 }
